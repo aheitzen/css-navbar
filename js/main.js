fb52f7239eef73722e6415f2b3f634cd30b6e9f9
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-10-06 17:23:31 -0700

    css navbar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $( "#slide-two" ).click(function() {
+  		$(this).animate( { left: '100px' }, 1000, 'easeOutQuad' );
+	});
+	$( "#slide-three" ).click(function() {
+  		$(this).animate( { left: '100px' }, 1000, 'easeOutQuad' );
+	});
+});
+
