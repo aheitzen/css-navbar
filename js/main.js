64be6c415c40adaed99808c348f738c5c03837fc
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-10-07 11:05:34 -0700

    navbar logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+var isSlideOneOpen = false
+var isSlideTwoOpen = false
+var isSlideThreeOpen = false
+
 $( document ).ready(function() {
 	$( "#slide-one" ).click(function() {
-  		$(this).animate( { left: '80px' }, 1000, 'easeOutQuad' );
+		console.log("CLICK", isSlideOneOpen);
+		if(!isSlideOneOpen) {
+  			$(this).animate( { left: '-=80%' }, 1000, 'easeOutQuad' );
+  		}
+  		else {
+  			$(this).animate( { left: '+=80%'}, 1000, 'easeOutQuad' );
+  		}
+  		isSlideOneOpen = !isSlideOneOpen
 	});
     $( "#slide-two" ).click(function() {
   		$(this).animate( { left: '80px' }, 1000, 'easeOutQuad' );
@@ -10,3 +21,18 @@ $( document ).ready(function() {
 	});
 });
 
+
+// left: '80px'
+// left: '-=50px', right: '420px'
+
+
+    // $("#slide-one").click(function () {
+    //     if ($(this).is(""))
+    //         $(this).animate( { left: '80px' }, 1000, 'easeOutQuad' );
+    //     else
+    //         $(this).animate( { left: '-80px' }, 1000, 'easeOutQuad' );
+    // });
+
+// $( "#slide-one" ).click(function() {
+//   $( "#slide-one" ).toggle( "fold", 1000 );
+// });
