0acd5a2cf0dc04dc7874001019ef077310524071
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-10-07 13:06:09 -0700

    navbar logic done

--- js/main.js
+++ js/main.js
@@ -135,91 +135,172 @@ $( document ).ready(function() {
 		}
 	});
 
-	// $( "#slide-three" ).click(function() {
+	$( "#slide-four" ).click(function() {
+		var hasChildrenOpen = false
+  		//if slide 1 is closed
+  		if(isSlideOneOpen === false) {
+			toggleSlide($( "#slide-one"), isSlideOneOpen);
+			isSlideOneOpen = true
+		}
+		if(isSlideTwoOpen === false) {
+			toggleSlide($( "#slide-two"), isSlideTwoOpen);
+			isSlideTwoOpen = true
+		}
+		if(isSlideThreeOpen === false) {
+			toggleSlide($( "#slide-three"), isSlideThreeOpen);
+			isSlideThreeOpen = true
+			// hasChildrenOpen = true
+		}
+		if(isSlideFiveOpen === true) {
+			toggleSlide($( "#slide-five"), isSlideFiveOpen);
+			isSlideFiveOpen = false
+			hasChildrenOpen = true
+		}
+		if(isSlideSixOpen === true) {
+			toggleSlide($( "#slide-six"), isSlideSixOpen);
+			isSlideSixOpen = false
+			hasChildrenOpen = true
+		}
+		if(isSlideSevenOpen === true) {
+			toggleSlide($( "#slide-seven"), isSlideSevenOpen);
+			isSlideSevenOpen = false
+			hasChildrenOpen = true
+		}
+		//if children are open do not close this tab
+		if(hasChildrenOpen === false) {
+			toggleSlide($( "#slide-four" ), isSlideFourOpen);
+			isSlideFourOpen = !isSlideFourOpen
+		}
+	});
+
+	$( "#slide-five" ).click(function() {
+		var hasChildrenOpen = false
+  		//if slide 1 is closed
+  		if(isSlideOneOpen === false) {
+			toggleSlide($( "#slide-one"), isSlideOneOpen);
+			isSlideOneOpen = true
+		}
+		if(isSlideTwoOpen === false) {
+			toggleSlide($( "#slide-two"), isSlideTwoOpen);
+			isSlideTwoOpen = true
+		}
+		if(isSlideThreeOpen === false) {
+			toggleSlide($( "#slide-three"), isSlideThreeOpen);
+			isSlideThreeOpen = true
+			// hasChildrenOpen = true
+		}
+		if(isSlideFourOpen === false) {
+			toggleSlide($( "#slide-four"), isSlideFourOpen);
+			isSlideFourOpen = true
+		}
+		if(isSlideSixOpen === true) {
+			toggleSlide($( "#slide-six"), isSlideSixOpen);
+			isSlideSixOpen = false
+			hasChildrenOpen = true
+		}
+		if(isSlideSevenOpen === true) {
+			toggleSlide($( "#slide-seven"), isSlideSevenOpen);
+			isSlideSevenOpen = false
+			hasChildrenOpen = true
+		}
+		//if children are open do not close this tab
+		if(hasChildrenOpen === false) {
+			toggleSlide($( "#slide-five" ), isSlideFiveOpen);
+			isSlideFiveOpen = !isSlideFiveOpen
+		}
+	});
+
+	$( "#slide-six" ).click(function() {
+		var hasChildrenOpen = false
+  		//if slide 1 is closed
+  		if(isSlideOneOpen === false) {
+			toggleSlide($( "#slide-one"), isSlideOneOpen);
+			isSlideOneOpen = true
+		}
+		if(isSlideTwoOpen === false) {
+			toggleSlide($( "#slide-two"), isSlideTwoOpen);
+			isSlideTwoOpen = true
+		}
+		if(isSlideThreeOpen === false) {
+			toggleSlide($( "#slide-three"), isSlideThreeOpen);
+			isSlideThreeOpen = true
+			// hasChildrenOpen = true
+		}
+		if(isSlideFourOpen === false) {
+			toggleSlide($( "#slide-four"), isSlideFourOpen);
+			isSlideFourOpen = true
+		}
+		if(isSlideFiveOpen === false) {
+			toggleSlide($( "#slide-five"), isSlideFiveOpen);
+			isSlideFiveOpen = true
+			// hasChildrenOpen = true
+		}
+		if(isSlideSevenOpen === true) {
+			toggleSlide($( "#slide-seven"), isSlideSevenOpen);
+			isSlideSevenOpen = false
+			hasChildrenOpen = true
+		}
+		//if children are open do not close this tab
+		if(hasChildrenOpen === false) {
+			toggleSlide($( "#slide-six" ), isSlideSixOpen);
+			isSlideSixOpen = !isSlideSixOpen
+		}
+	});
+
+	$( "#slide-seven" ).click(function() {
+		var hasChildrenOpen = false
+  		//if slide 1 is closed
+  		if(isSlideOneOpen === false) {
+			toggleSlide($( "#slide-one"), isSlideOneOpen);
+			isSlideOneOpen = true
+		}
+		if(isSlideTwoOpen === false) {
+			toggleSlide($( "#slide-two"), isSlideTwoOpen);
+			isSlideTwoOpen = true
+		}
+		if(isSlideThreeOpen === false) {
+			toggleSlide($( "#slide-three"), isSlideThreeOpen);
+			isSlideThreeOpen = true
+			// hasChildrenOpen = true
+		}
+		if(isSlideFourOpen === false) {
+			toggleSlide($( "#slide-four"), isSlideFourOpen);
+			isSlideFourOpen = true
+		}
+		if(isSlideFiveOpen === false) {
+			toggleSlide($( "#slide-five"), isSlideFiveOpen);
+			isSlideFiveOpen = true
+			// hasChildrenOpen = true
+		}
+		if(isSlideSixOpen === false) {
+			toggleSlide($( "#slide-six"), isSlideSixOpen);
+			isSlideSixOpen = true
+			// hasChildrenOpen = true
+		}
+		//if children are open do not close this tab
+		if(hasChildrenOpen === false) {
+			toggleSlide($( "#slide-seven" ), isSlideSevenOpen);
+			isSlideSevenOpen = !isSlideSevenOpen
+		}
+	});
+
+
+
+
+
+
+
+	// $( "#slide-seven" ).click(function() {
 	// 	// console.log("CLICK", isSlideOneOpen);
-	// 	if(!isSlideThreeOpen) {
+	// 	if(!isSlideSevenOpen) {
  //  			$(this).animate( { left: '-=60%' }, 1000, 'easeOutQuad' );
  //  		}
  //  		else {
  //  			$(this).animate( { left: '+=60%'}, 1000, 'easeOutQuad' );
  //  		}
- //  		isSlideThreeOpen = !isSlideThreeOpen
+ //  		isSlideSevenOpen = !isSlideSevenOpen
 	// });
 
-
-
-
-
-
-
-
-
-	
-	
-
-
-
-	
-	$( "#slide-four" ).click(function() {
-		// console.log("CLICK", isSlideOneOpen);
-		if(!isSlideFourOpen) {
-  			$(this).animate( { left: '-=60%' }, 1000, 'easeOutQuad' );
-  		}
-  		else {
-  			$(this).animate( { left: '+=60%'}, 1000, 'easeOutQuad' );
-  		}
-  		isSlideFourOpen = !isSlideFourOpen
-	});
-	$( "#slide-five" ).click(function() {
-		// console.log("CLICK", isSlideOneOpen);
-		if(!isSlideFiveOpen) {
-  			$(this).animate( { left: '-=60%' }, 1000, 'easeOutQuad' );
-  		}
-  		else {
-  			$(this).animate( { left: '+=60%'}, 1000, 'easeOutQuad' );
-  		}
-  		isSlideFiveOpen = !isSlideFiveOpen
-	});
-	$( "#slide-six" ).click(function() {
-		// console.log("CLICK", isSlideOneOpen);
-		if(!isSlideSixOpen) {
-  			$(this).animate( { left: '-=60%' }, 1000, 'easeOutQuad' );
-  		}
-  		else {
-  			$(this).animate( { left: '+=60%'}, 1000, 'easeOutQuad' );
-  		}
-  		isSlideSixOpen = !isSlideSixOpen
-	});
-	$( "#slide-seven" ).click(function() {
-		// console.log("CLICK", isSlideOneOpen);
-		if(!isSlideSevenOpen) {
-  			$(this).animate( { left: '-=60%' }, 1000, 'easeOutQuad' );
-  		}
-  		else {
-  			$(this).animate( { left: '+=60%'}, 1000, 'easeOutQuad' );
-  		}
-  		isSlideSevenOpen = !isSlideSevenOpen
-	});
- //    $( "#slide-two" ).click(function() {
- //  		$(this).animate( { left: '80px' }, 1000, 'easeOutQuad' );
-	// });
-	// $( "#slide-three" ).click(function() {
- //  		$(this).animate( { left: '80px' }, 1000, 'easeOutQuad' );
-	// });
 });
 
 
-// left: '80px'
-// left: '-=50px', right: '420px'
-
-
-    // $("#slide-one").click(function () {
-    //     if ($(this).is(""))
-    //         $(this).animate( { left: '80px' }, 1000, 'easeOutQuad' );
-    //     else
-    //         $(this).animate( { left: '-80px' }, 1000, 'easeOutQuad' );
-    // });
-
-// $( "#slide-one" ).click(function() {
-//   $( "#slide-one" ).toggle( "fold", 1000 );
-// });
